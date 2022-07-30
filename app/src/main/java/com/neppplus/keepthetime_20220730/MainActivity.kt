4f6cf031575311e39667a756c7db6eecefb68b33
김민철 <kmc901206@gmail.com>
2022-07-30 13:41:13 +0900

    ViewPager 태그와 어댑터를 연결

--- app/src/main/java/com/neppplus/keepthetime_20220730/MainActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/MainActivity.kt
@@ -2,10 +2,29 @@ package com.neppplus.keepthetime_20220730
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20220730.adapters.MainViewPagerAdapter
+import com.neppplus.keepthetime_20220730.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+
+    lateinit var mBinding : ActivityMainBinding
+
+    lateinit var mPagerAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+        mPagerAdapter = MainViewPagerAdapter(this)
+        mBinding.mainViewPager.adapter = mPagerAdapter
     }
 }
