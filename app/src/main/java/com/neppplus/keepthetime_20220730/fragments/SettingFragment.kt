fc92faad582827ac356c95e7391a5517149e54d6
김민철 <kmc901206@gmail.com>
2022-07-30 15:05:35 +0900

    로그아웃 기능 구현

--- app/src/main/java/com/neppplus/keepthetime_20220730/fragments/SettingFragment.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/fragments/SettingFragment.kt
@@ -1,5 +1,8 @@
 package com.neppplus.keepthetime_20220730.fragments
 
+import android.app.AlertDialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,8 +10,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.neppplus.keepthetime_20220730.LoginActivity
+import com.neppplus.keepthetime_20220730.MainActivity
 import com.neppplus.keepthetime_20220730.R
 import com.neppplus.keepthetime_20220730.databinding.FragmentSettingBinding
+import com.neppplus.keepthetime_20220730.utils.ContextUtil
 import com.neppplus.keepthetime_20220730.utils.GlobalData
 
 class SettingFragment : BaseFragment() {
@@ -31,7 +37,26 @@ class SettingFragment : BaseFragment() {
     }
 
     override fun setupEvents() {
+//        로그아웃
+        mBinding.logoutLayout.setOnClickListener {
+            val alert = AlertDialog.Builder(mContext)
+                .setTitle("로그아웃")
+                .setMessage("로그아웃 하시겠습니까?")
+                .setPositiveButton("확인", DialogInterface.OnClickListener { dialogInterface, i ->
+//                    로그아웃 로직 진행
 
+                    ContextUtil.setLoginToken(mContext, "")
+                    GlobalData.loginUser = null
+
+                    val myIntent = Intent(mContext, LoginActivity::class.java)
+                    startActivity(myIntent)
+                    requireActivity().finish()
+                })
+                .setNegativeButton("취소", null)
+                .show()
+
+
+        }
     }
 
     override fun setValues() {
