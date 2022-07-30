5df2a7d1eade4897c87cce44f566324e3d6855c2
김민철 <kmc901206@gmail.com>
2022-07-30 10:16:35 +0900

    BaseActivity 생성 및 로그인 액티비티 kt파일 기초 작업

--- app/src/main/java/com/neppplus/keepthetime_20220730/LoginActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/LoginActivity.kt
@@ -2,10 +2,25 @@ package com.neppplus.keepthetime_20220730
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20220730.databinding.ActivityLoginBinding
 
-class LoginActivity : AppCompatActivity() {
+class LoginActivity : BaseActivity() {
+
+    lateinit var mBinding : ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_login)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
