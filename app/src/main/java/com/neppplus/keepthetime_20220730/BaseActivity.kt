1a859c3f9efe2d4ea5c17c63a873d0a98566995e
김민철 <kmc901206@gmail.com>
2022-07-31 09:44:45 +0900

    customActionBar 장착

--- app/src/main/java/com/neppplus/keepthetime_20220730/BaseActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/BaseActivity.kt
@@ -2,7 +2,10 @@ package com.neppplus.keepthetime_20220730
 
 import android.content.Context
 import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import com.neppplus.keepthetime_20220730.api.APIList
 import com.neppplus.keepthetime_20220730.api.ServerApi
 import retrofit2.Retrofit
@@ -15,16 +18,34 @@ abstract class BaseActivity : AppCompatActivity(){
     lateinit var retrofit : Retrofit
     lateinit var apiList : APIList
 
+    lateinit var titleTxt : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
 
         retrofit = ServerApi.getRetrofit(mContext)
         apiList = retrofit.create(APIList::class.java)
+
+        supportActionBar?.let {
+            setCustomActionBar()
+        }
     }
 
     abstract fun setupEvents()
 
     abstract fun setValues()
+
+    fun setCustomActionBar () {
+        val defActionBar = supportActionBar!!
+
+        defActionBar.displayOptions = ActionBar.DISPLAY_SHOW_CUSTOM
+        defActionBar.setCustomView(R.layout.custom_action_bar)
+
+        val toolbar = defActionBar.customView.parent as Toolbar
+        toolbar.setContentInsetsAbsolute(0,0)
+
+        titleTxt = defActionBar.customView.findViewById(R.id.titleTxt)
+    }
 
 }
