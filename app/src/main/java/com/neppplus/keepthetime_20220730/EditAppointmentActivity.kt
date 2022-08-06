1980f2005cf2fc806a884fbe0a0a3c4f5ccbb4b1
김민철 <kmc901206@gmail.com>
2022-08-06 15:02:32 +0900

    약속 생성 Activity xml 작업

---
+++ app/src/main/java/com/neppplus/keepthetime_20220730/EditAppointmentActivity.kt
@@ -0,0 +1,11 @@
+package com.neppplus.keepthetime_20220730
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditAppointmentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_appointment_acitivty)
+    }
+}
