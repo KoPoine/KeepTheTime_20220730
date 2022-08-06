1f991011444fcc998d33b0c278a656e45c4934b8
김민철 <kmc901206@gmail.com>
2022-08-06 15:03:57 +0900

    코틀린 기본 작업

--- app/src/main/java/com/neppplus/keepthetime_20220730/EditAppointmentActivity.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/EditAppointmentActivity.kt
@@ -1,11 +1,25 @@
 package com.neppplus.keepthetime_20220730
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.neppplus.keepthetime_20220730.databinding.ActivityEditAppointmentAcitivtyBinding
 
-class EditAppointmentActivity : AppCompatActivity() {
+class EditAppointmentActivity : BaseActivity() {
+
+    lateinit var mBinding : ActivityEditAppointmentAcitivtyBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_edit_appointment_acitivty)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_edit_appointment_acitivty)
+        setupEvents()
+        setValues()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
