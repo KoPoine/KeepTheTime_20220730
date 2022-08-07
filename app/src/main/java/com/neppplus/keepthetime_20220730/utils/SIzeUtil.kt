26c30069d65396adf32e7cabae90c9c8df0c8e09
넵플러스 <kmc901206@gmail.com>
2022-08-07 11:36:15 +0900

    sizeUtil 일부 수정

--- app/src/main/java/com/neppplus/keepthetime_20220730/utils/SIzeUtil.kt
+++ app/src/main/java/com/neppplus/keepthetime_20220730/utils/SIzeUtil.kt
@@ -6,8 +6,16 @@ import android.util.TypedValue
 class SizeUtil {
 
     companion object {
-        fun dpToPx(context : Context, dp : Float) : Float {
-            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics)
+        fun dpToPx(context : Context, dp : Float) : Int {
+            return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.resources.displayMetrics).toInt()
+        }
+
+        fun ConvertSPtoPX(context: Context, sp: Int): Int {
+            return TypedValue.applyDimension(
+                TypedValue.COMPLEX_UNIT_SP,
+                sp.toFloat(),
+                context.resources.displayMetrics
+            ).toInt()
         }
     }
 
